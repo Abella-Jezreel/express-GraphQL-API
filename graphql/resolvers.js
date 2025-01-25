66f595fbafaa58fbe3c1bfdc96ee8e531c881238
Abella-Jezreel <teamabella@outlook.com>
2025-01-25 15:05:32 +0800

    Connecting the Frontend to the GraphQL API

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -5,19 +5,25 @@ const validator = require("validator");
 module.exports = {
     createUser: async function ({ userInput }, req) {
         // const email = args.userInput.email;
+        const errors = [];
         if (!validator.isEmail(userInput.email)) {
-            const error = new Error("E-Mail is invalid.");
+            errors.push({ message: "E-Mail is invalid." });
+        }
+        if (
+            validator.isEmpty(userInput.password) ||
+            !validator.isLength(userInput.password, { min: 5 })
+        ) {
+            errors.push({ message: "Password too short!" });
+        }
+        if (errors.length > 0) {
+            const error = new Error("Invalid input.");
+            error.data = errors;
+            error.code = 422;
             throw error;
         }
-        if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, { min: 5 })) {
-            const error = new Error("Password too short!");
-            throw error;
-        }
-        if (validator.isEmpty(userInput.name)) {
-            const error = new Error("Name is invalid.");
-            throw error;
-        }
-        const existingUser = await User.findOne({ email: userInput.email });
+        const existingUser = await User.findOne({
+            email: userInput.email,
+        });
         if (existingUser) {
             const error = new Error("User exists already!");
             throw error;
